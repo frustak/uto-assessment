fb0c5ca137ccd5aaf1312a56fa93a735ac80ca48
frustak <soulburn007@gmail.com>
2021-07-27 20:57:05 +0430

    add mock post edit

---
+++ src/routes/post-edit/index.tsx
@@ -0,0 +1,57 @@
+import { Box, Button, makeStyles, TextField } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        paddingTop: 20,
+    },
+    form: {
+        paddingBottom: 80,
+    },
+    input: {
+        margin: "10px 0",
+    },
+});
+
+function PostEdit() {
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.root}>
+            <form autoComplete="off" className={classes.form}>
+                <TextField
+                    variant="outlined"
+                    className={classes.input}
+                    fullWidth
+                    helperText="This field is required"
+                    label="Title"
+                    error
+                />
+                <TextField
+                    variant="outlined"
+                    className={classes.input}
+                    fullWidth
+                    helperText="This field is required"
+                    multiline
+                    label="Content"
+                    rows={2}
+                    maxRows={20}
+                />
+            </form>
+
+            <Box
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-between"
+            >
+                <Button variant="contained" size="large" color="secondary">
+                    CANCEL
+                </Button>
+                <Button variant="contained" size="large" color="primary">
+                    SAVE
+                </Button>
+            </Box>
+        </Box>
+    );
+}
+
+export default PostEdit;
