d45b437cb6157e0573e8f740fc3194fdb94db55e
frustak <soulburn007@gmail.com>
2021-07-27 17:22:14 +0430

    Setup routes and store

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Edit from "./routes/Edit";
+import Home from "./routes/Home";
 
 function App() {
     return (
         <>
             <CssBaseline />
+
+            <Router>
+                <Switch>
+                    <Route path="/edit">
+                        <Edit />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
         </>
     );
 }
