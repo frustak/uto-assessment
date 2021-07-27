95d7e11849b8de44783677b74c8de83446dd9aeb
frustak <soulburn007@gmail.com>
2021-07-27 23:43:50 +0430

    implement redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postsReducer from "./posts-slice";
 
-export default configureStore({
+const store = configureStore({
     reducer: {
-        dummy: () => null,
+        posts: postsReducer,
     },
 });
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ src/store/posts-slice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AppDispatch } from ".";
+import * as api from "../api";
+import { Post } from "../api/schemas";
+
+type PostsState = {
+    items: Post[];
+};
+
+const initialState: PostsState = {
+    items: [],
+};
+
+export const postsSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {
+        setPosts: (state, action: PayloadAction<Post[]>) => {
+            state.items = action.payload;
+        },
+    },
+});
+
+export const { setPosts } = postsSlice.actions;
+
+const fetchPosts = () => async (dispatch: AppDispatch) => {
+    const posts = await api.getPosts();
+    dispatch(setPosts(posts));
+};
+
+export default postsSlice.reducer;
