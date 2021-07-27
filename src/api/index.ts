237f47976b05e9cbc3b71b52ba997d2c27a7b76a
frustak <soulburn007@gmail.com>
2021-07-27 23:43:22 +0430

    fix typo

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ const instance = axios.create({
     baseURL: process.env.REACT_APP_SERVER_ADDRESS,
 });
 
-export async function getPosts(): Promise<[Post]> {
+export async function getPosts(): Promise<Post[]> {
     const response = await instance.get("/posts");
     return response.data;
 }
