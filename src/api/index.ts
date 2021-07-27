838db49b663a01e19cff261bf6d41557c4bdf072
frustak <soulburn007@gmail.com>
2021-07-27 22:51:50 +0430

    add api integeration

---
+++ src/api/index.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { Post } from "./schemas";
+
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_SERVER_ADDRESS,
+});
+
+export async function getPosts(): Promise<[Post]> {
+    const response = await instance.get("/posts");
+    return response.data;
+}
+
+export async function updatePost(newPost: Post): Promise<Post> {
+    const response = await instance.put(`/posts/${newPost.id}`, newPost);
+    return response.data;
+}
